901bad283cc589c0a2c207cf279cb7f1fd0bc6cf
ThatCreep2089 <76827300+ThatCreep2089@users.noreply.github.com>
2024-01-28 22:00:30 +0100

    Revert "input nuevo (no funca pero xd)"
    This reverts commit 66079975ad40b83d9b8a4322d3241805f8886ace.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,67 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset playerInput;
+    private float xAxis;
+    private float yAxis;
     private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 10f;
-    private InputAction moveInput ;
-    void Start() //Awake()
+    public float moveSpeed = 10f;
+    private Vector2 direction;
+    public void SetHorizontalInput(float x)
     {
-        playerInput = GetComponent<InputActionAsset>();
+        xAxis = x;
+    }
+    public void SetVerticalInput(float y)
+    {
+        yAxis = y;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-
     }
-
-    private void OnEnable()
-    {
-        playerInput.Enable();
-    }
-    private void OnDisable()
-    {
-        playerInput.Disable();
-    }
-
-
+    
+    // Update is called once per frame
     void FixedUpdate()
     {
-        //Vector2 moveInput = playerInput.PlayerMovement.Move.ReadValue<Vector2>();
-        var map = playerInput.FindActionMap("PlayerMovement");
-        moveInput = map.FindAction("Move");
-        moveInput.performed += ReadInput;
-        moveInput.canceled += ReadInput;
-        
+        direction = new Vector2(xAxis, yAxis).normalized;
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
     }
-    private void ReadInput(InputAction.CallbackContext context) 
-    { 
-        var input = context.ReadValue<Vector2>();
-        rb.velocity = input * moveSpeed;
-    }
-    /*private float xAxis;
-   private float yAxis;
-   private Vector2 direction;
-   public void SetHorizontalInput(float x)
-   {
-       xAxis = x;
-   }
-   public void SetVerticalInput(float y)
-   {
-       yAxis = y;
-   }
-
-   // Start is called before the first frame update
-   void Start()
-   {
-       rb = GetComponent<Rigidbody2D>();
-   }
-
-   // Update is called once per frame
-   void FixedUpdate()
-   {
-       direction = new Vector2(xAxis, yAxis).normalized;
-       rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
-   }*/
 }
