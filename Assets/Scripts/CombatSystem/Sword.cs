078d41b1fdfce19a247c5435fd88a7a1f588e07c
Alec <149337496+fwoggydweller@users.noreply.github.com>
2024-01-31 17:11:19 +0100

    Animaciones de espada

--- Assets/Scripts/CombatSystem/Sword.cs
+++ Assets/Scripts/CombatSystem/Sword.cs
@@ -10,27 +10,10 @@ public class Sword : MonoBehaviour, IWeapon
     private GameObject swordHitbox;
     private GameObject currentHitbox;
     private PlayerAnimatorController playerAnimatorController;
-
-    private bool _attacking;
-    public bool attacking
-    {
-        get
-        {
-            return _attacking;
-        }
-        set
-        {
-            _attacking = value;
-            playerAnimatorController.IsAttacking(value);
-        }
-    } //cooldown de atacar. EL ANIMATOR ACCEDE A ESTA VARIABLE PARA COMPROBAR SI ESTA ATACANDO O NO
     void Start()
     {
         playerAnimatorController = GetComponent<PlayerAnimatorController>();
     }
-    public void Use(Vector2 playerPosition, Vector2 playerDirection)
-    PlayerAnimatorController playerAnimatorController;
-
 
     public bool Use(Vector2 playerPosition, Vector2 playerDirection)
     {
@@ -38,18 +21,13 @@ public class Sword : MonoBehaviour, IWeapon
 
         playerAnimatorController.onUsingItem(true, 0);
 
-            currentHitbox =Instantiate(swordHitbox, playerPosition + playerDirection, Quaternion.identity);
-            currentHitbox.GetComponent<SwordHitbox>().playerCollider = GetComponent<Collider2D>();
+        currentHitbox =Instantiate(swordHitbox, playerPosition + playerDirection, Quaternion.identity);
+        currentHitbox.GetComponent<SwordHitbox>().playerCollider = GetComponent<Collider2D>();
         currentHitbox.GetComponent<SwordHitbox>()._animatorController = GetComponent<PlayerAnimatorController>();
 
-            //Checkear si Link está con la vida al completo. Si lo está, disparar un prefab de proyectil.
-        }
+        //Checkear si Link está con la vida al completo. Si lo está, disparar un prefab de proyectil
         //Checkear si Link está con la vida al completo. Si lo está, disparar un prefab de proyectil.
-
+ 
         return true;
-    }
-    private void Start()
-    {
-        playerAnimatorController = GetComponent<PlayerAnimatorController>();
     }
 }
--- Assets/Scripts/Player/PlayerAnimatorController.cs
+++ Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -21,16 +21,10 @@ public class PlayerAnimatorController : MonoBehaviour
         {
             _myAnimator.SetBool("IsUsingItem", boolean);
         }
-
         Debug.Log(boolean + " "+actionType);
-        
     }
 
     // Start is called before the first frame update
-    public void IsAttacking(bool attack)
-    {
-        attacking = attack;
-    }
     void Start()
     {
         _myAnimator = GetComponent<Animator>();
@@ -40,81 +34,51 @@ public class PlayerAnimatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(attacking)
+        if (rb.velocity.y > 0.1f)
         {
-            if (xAxis == 0 && yAxis == 1)
-            {
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (xAxis == 0 && yAxis == -1)
-            {
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (xAxis == 1 && yAxis == 0)
-            {
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (xAxis == -1 && yAxis == 0)
-            {
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
+            xAxis = 0;
+            yAxis = 1;
+            isMooving = true;
+
+            _myAnimator.SetBool("IsMooving", isMooving);
+            _myAnimator.SetInteger("xInput", xAxis);
+            _myAnimator.SetInteger("yInput", yAxis);
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            xAxis = 0;
+            yAxis = -1;
+            isMooving = true;
+
+            _myAnimator.SetBool("IsMooving", isMooving);
+            _myAnimator.SetInteger("xInput", xAxis);
+            _myAnimator.SetInteger("yInput", yAxis);
+        }
+        else if (rb.velocity.x > 0.1f)
+        {
+            xAxis = 1;
+            yAxis = 0;
+            isMooving = true;
+
+            _myAnimator.SetBool("IsMooving", isMooving);
+            _myAnimator.SetInteger("xInput", xAxis);
+            _myAnimator.SetInteger("yInput", yAxis);
+        }
+        else if (rb.velocity.x < -0.1f)
+        {
+            xAxis = -1;
+            yAxis = 0;
+            isMooving = true;
+
+            _myAnimator.SetBool("IsMooving", isMooving);
+            _myAnimator.SetInteger("xInput", xAxis);
+            _myAnimator.SetInteger("yInput", yAxis);
         }
         else
         {
-            if (rb.velocity.y > 0.1f)
-            {
-                xAxis = 0;
-                yAxis = 1;
-                isMooving = true;
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetBool("IsMooving", isMooving);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (rb.velocity.y < -0.1f)
-            {
-                xAxis = 0;
-                yAxis = -1;
-                isMooving = true;
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetBool("IsMooving", isMooving);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (rb.velocity.x > 0.1f)
-            {
-                xAxis = 1;
-                yAxis = 0;
-                isMooving = true;
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetBool("IsMooving", isMooving);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else if (rb.velocity.x < -0.1f)
-            {
-                xAxis = -1;
-                yAxis = 0;
-                isMooving = true;
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetBool("IsMooving", isMooving);
-                _myAnimator.SetInteger("xInput", xAxis);
-                _myAnimator.SetInteger("yInput", yAxis);
-            }
-            else
-            {
-                isMooving = false;
-                _myAnimator.SetBool("Attacking", attacking);
-                _myAnimator.SetBool("IsMooving", isMooving);
-            }
+            isMooving = false;
+
+            _myAnimator.SetBool("IsMooving", isMooving);
         }
     }
 }
