01c8b887957b0d3bf0366528bf0dfe7f31d1a9f7
Lparres2000 <Lparres2000@gmail.com>
2024-02-04 23:54:45 +0100

    Merge branch 'develop' of https://github.com/ChichoRD/MAP-World-Inc into develop

--- Assets/Scripts/HealthSystem/HealthSystem.cs
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -1,43 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthSystem : MonoBehaviour
 
 
 {
+
+    public UnityEvent<int> OnHealthChange = new UnityEvent<int>();
+
     #region parameters 
 
-    public int health; 
     public int maxhealth;
+    int health;
+    public int currenthealth
+    {
+        get { return health; }
+        set
+        {
+            OnHealthChange?.Invoke(value);
+            health = value;
+            
+        }
 
-    //Cada 1 de vida es medio corazón, así que link tiene 6 de vida. Algunos enemigos tienen 1 o 2.
+    }
+
+
+    //Cada 1 de vida es medio corazï¿½n, asï¿½ que link tiene 6 de vida. Algunos enemigos tienen 1 o 2.
     #endregion
 
     #region methods
 
-    //Recibe daño y se le resta a la vida, vale para link y para los enemigos.
-    public void LoseHealth(int DamageTaken)  
+    //Recibe daï¿½o y se le resta a la vida, vale para link y para los enemigos.
+    public void LoseHealth(int DamageTaken)
     {
         health -= DamageTaken;
 
         if (health <= 0)
         {
-            gameObject.SetActive(false); 
+            gameObject.SetActive(false);
         }
 
     }
 
-    //Recupera 2 de vida equivalente a 1 corazón (específicamente para link) cuando hay colisión con los corazones que dropean a veces los enemigos.
-    public void Heal()  
+    //Recupera 2 de vida equivalente a 1 corazï¿½n (especï¿½ficamente para link) cuando hay colisiï¿½n con los corazones que dropean a veces los enemigos.
+    public void Heal()
     {
-        health+=2; 
+        health += 2;
 
-        if (health >= maxhealth ) { health = maxhealth; }
+        if (health >= maxhealth) { health = maxhealth; }
     }
 
-    //Aumenta la vida máxima, es el item de la sala secreta que abres con la bomba. Se llama este método si hay colisión entre el item y link.
-    public void ContainerHeart() 
+    //Aumenta la vida mï¿½xima, es el item de la sala secreta que abres con la bomba. Se llama este mï¿½todo si hay colisiï¿½n entre el item y link.
+    public void ContainerHeart()
     {
         maxhealth += 2;
         health += 2;
@@ -45,13 +61,13 @@ public class HealthSystem : MonoBehaviour
 
     #endregion
 
-    
+
     void Start()
     {
         health = maxhealth;
     }
 
-    
+
     void Update()
     {
 
