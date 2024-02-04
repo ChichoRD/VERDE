48187c52a0a8844499ae0edd092757e914805dbb
Diegoucm <dimart15@ucm.es>
2024-02-04 15:51:01 +0100

    Evento healthchange y enemy health

--- Assets/Scripts/HealthSystem/HealthSystem.cs
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthSystem : MonoBehaviour
 
 
 {
+
+    public UnityEvent<int> OnHealthChange = new UnityEvent<int>();
+
     #region parameters 
 
-    public int health; 
     public int maxhealth;
+    int health;
+    public int currenthealth
+    {
+        get { return health; }
+        set
+        {
+            OnHealthChange?.Invoke(value);
+            health = value;
+            
+        }
+
+    }
+
 
     //Cada 1 de vida es medio corazón, así que link tiene 6 de vida. Algunos enemigos tienen 1 o 2.
     #endregion
@@ -17,27 +33,27 @@ public class HealthSystem : MonoBehaviour
     #region methods
 
     //Recibe daño y se le resta a la vida, vale para link y para los enemigos.
-    public void LoseHealth(int DamageTaken)  
+    public void LoseHealth(int DamageTaken)
     {
         health -= DamageTaken;
 
         if (health <= 0)
         {
-            gameObject.SetActive(false); 
+            gameObject.SetActive(false);
         }
 
     }
 
     //Recupera 2 de vida equivalente a 1 corazón (específicamente para link) cuando hay colisión con los corazones que dropean a veces los enemigos.
-    public void Heal()  
+    public void Heal()
     {
-        health+=2; 
+        health += 2;
 
-        if (health >= maxhealth ) { health = maxhealth; }
+        if (health >= maxhealth) { health = maxhealth; }
     }
 
     //Aumenta la vida máxima, es el item de la sala secreta que abres con la bomba. Se llama este método si hay colisión entre el item y link.
-    public void ContainerHeart() 
+    public void ContainerHeart()
     {
         maxhealth += 2;
         health += 2;
@@ -45,13 +61,13 @@ public class HealthSystem : MonoBehaviour
 
     #endregion
 
-    
+
     void Start()
     {
         health = maxhealth;
     }
 
-    
+
     void Update()
     {
 
