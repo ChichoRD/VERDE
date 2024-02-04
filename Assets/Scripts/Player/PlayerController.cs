56a573f3bd9cbe3e1f35ce815f615e52d6d967da
Lparres2000 <Lparres2000@gmail.com>
2024-02-04 23:53:44 +0100

    Nose un monton de cosas

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -26,63 +26,29 @@ public class PlayerController : MonoBehaviour
     public void ReadInput(Vector2 input) 
     {
 
-            // Versi�n 1 con matem�ticas
-            /**
-            Vector2 base0 = Vector2.right;
-            Vector2 base1 = Vector2.up;
+        if(true) {
 
-            float base0Proyection = Vector3.Dot(input, base0);
-            float base1Proyection = Vector3.Dot(input, base1);
-            
-            
-            Vector2 orto = Vector2.zero;
-
-
-            if (Mathf.Abs(base0Proyection) > Mathf.Abs(base1Proyection)) orto = base0 * base0Proyection;
-            else orto = base1 * base1Proyection;
-
-       
-        
-            rb.velocity = orto.normalized * moveSpeed;
-        
-        
-
-            if(orto.magnitude > 0)
-            {
-                lookDirection = orto.normalized;
+            if (rb.velocity.x != 0 && input.y != 0) {
+                rb.velocity = new Vector2(0, input.y > 0 ? 1 : -1) * moveSpeed;
+            } else if (rb.velocity.y != 0 && input.x != 0) {
+                rb.velocity = new Vector2(input.x > 0 ? 1 : -1, 0) * moveSpeed;
+            } else if (input.x != 0) {
+                rb.velocity = new Vector2(input.x > 0 ? 1 : -1, 0) * moveSpeed;
+            } else if (input.y != 0) {
+                rb.velocity = new Vector2(0, input.y > 0 ? 1 : -1) * moveSpeed;
+            } else {
+                rb.velocity = Vector2.zero;
             }
 
-            **/
-            // Versi�n 2 con ifs
-
-            if(rb.velocity.x != 0 && input.y != 0)
-            {
-               rb.velocity = new Vector2(0, input.y > 0 ? 1 : -1) * moveSpeed;
-            }
-
-            else if(rb.velocity.y != 0 && input.x != 0)
-            {
-               rb.velocity = new Vector2(input.x > 0 ? 1 : -1, 0) * moveSpeed;
-            }
-
-            else if(input.x != 0)
-            {
-               rb.velocity = new Vector2(input.x > 0 ? 1 : -1, 0) * moveSpeed;
-            }
-            else if(input.y != 0)
-            {
-               rb.velocity = new Vector2(0, input.y > 0 ? 1 : -1) * moveSpeed;
-            }
-            else
-            {
-               rb.velocity = Vector2.zero;
-            }
-
-            if(rb.velocity != Vector2.zero) {
-               lookDirection = rb.velocity.normalized;
+            if (rb.velocity != Vector2.zero) {
+                lookDirection = rb.velocity.normalized;
             }
 
             Debug.Log(lookDirection);
+
+        }
+
+            
 
     }
 }
