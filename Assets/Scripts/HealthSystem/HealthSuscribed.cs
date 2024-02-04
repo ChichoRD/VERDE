4779e542bdbe1a27368f7806e09d64c0065f8c28
Diegoucm <dimart15@ucm.es>
2024-02-04 16:23:49 +0100

    Healthsystem definitivo

---
+++ Assets/Scripts/HealthSystem/HealthSuscribed.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthSu : MonoBehaviour
+{
+    [SerializeField]
+    LinkStats _linkStats;
+    [SerializeField]
+    HealthSystem _healthSystem;
+    public void HealthSuscribe(int newhealth)
+    {
+        
+        _linkStats.currentHealth = newhealth ;
+    }
+
+    void Start()
+    {
+        _healthSystem = GetComponent<HealthSystem>();
+        _healthSystem.OnHealthChange.AddListener(HealthSuscribe);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
