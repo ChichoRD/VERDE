e9d6bbf4f202489bc2e3cc2524cf9a7fac2ed9c7
Javipopotter <javivals@ucm.es>
2024-02-05 22:07:40 +0100

    Done

--- Assets/Scripts/CollectionSystem/LinkStats.cs
+++ Assets/Scripts/CollectionSystem/LinkStats.cs
@@ -1,15 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 [CreateAssetMenu(fileName = "LinkStats", menuName = "MAP-World-Inc/LinkStats", order = 0)]
 public class LinkStats : ScriptableObject 
 {
-    public int currentHealth;
-    public int bombCount;
-    public int rupeeCount;
-    public int keyCount;
+    int _currentHealth;
+    public int currentHealth
+    {
+        get { return _currentHealth; }
+        set
+        {
+            _currentHealth = value;
+            OnHealthChange.Invoke(_currentHealth);
+        }
+    }
+
+    int _bombCount;
+    public int bombCount
+    {
+        get { return _bombCount; }
+        set
+        {
+            _bombCount = value;
+            OnBombChange.Invoke(_bombCount);
+        }
+    }
+
+    int _rupeeCount;
+    public int rupeeCount
+    {
+        get { return _rupeeCount; }
+        set
+        {
+            _rupeeCount = value;
+            OnRupeeChange.Invoke(_rupeeCount);
+        }
+    }
+
+    int _keyCount;
+    public int keyCount
+    {
+        get { return _keyCount; }
+        set
+        {
+            _keyCount = value;
+            OnKeyChange.Invoke(_keyCount);
+        }
+    }
+
+    public UnityEvent<int> OnHealthChange = new UnityEvent<int>();
+    public UnityEvent<int> OnBombChange = new UnityEvent<int>();
+    public UnityEvent<int> OnRupeeChange = new UnityEvent<int>();
+    public UnityEvent<int> OnKeyChange = new UnityEvent<int>();
+
 
     public void Reset()
     {
